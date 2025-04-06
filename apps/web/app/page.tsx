2a230d4d7110bb6fd015adbabe5aa454e88f7846
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-06 18:04:50 -0400

    Remove create-next-app demo code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
-import styles from "./page.module.css";
-
 export default function Home() {
-	return <div className={styles.page}></div>;
+	return (
+		<div>
+			<p>Hello world</p>
+		</div>
+	);
 }
