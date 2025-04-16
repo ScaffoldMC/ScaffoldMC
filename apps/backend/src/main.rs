751c08588af99b55dad0ec41a867ce5bef6bc19f
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-15 23:58:13 -0400

    Create a logger

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,9 +1,18 @@
+mod logger;
 mod routes;
 
 use axum::Router;
+use log::{info, LevelFilter};
+
+static LOGGER: logger::Logger = logger::Logger;
 
 #[tokio::main]
 async fn main() {
+	log::set_logger(&LOGGER)
+		.map(|()| log::set_max_level(LevelFilter::Info))
+		.expect("Failed to set logger");
+
+	info!("Starting server...");
 	let app: Router = Router::new().merge(routes::create_router());
 	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 	axum::serve(listener, app).await.unwrap();
