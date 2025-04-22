3b1172b6313e386a2735d174e00d8f84ff9610d8
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-21 20:43:27 -0400

    Create avatar component

---
+++ apps/web/components/atoms/avatar/avatar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import styles from "./avatar.module.css";
+
+export function Avatar({
+	...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
+	return <AvatarPrimitive.Root className={styles.base} {...props} />;
+}
+
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+export function AvatarImage({
+	...props
+}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+	return <AvatarPrimitive.Image {...props} />;
+}
+
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+export function AvatarFallback({
+	...props
+}: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
+	return <AvatarPrimitive.Fallback className={styles.fallback} {...props} />;
+}
+
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
