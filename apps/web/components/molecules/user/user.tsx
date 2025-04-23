e7de156b5a802d5699d877bd3cf5509f9ae849c9
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-22 21:42:51 -0400

    Add logout button

--- apps/web/components/molecules/user/user.tsx
+++ apps/web/components/molecules/user/user.tsx
@@ -1,18 +1,22 @@
 import { Avatar, AvatarFallback } from "@/components/atoms/avatar/avatar";
 import styles from "./user.module.css";
+import { LogOut } from "lucide-react";
+import { Button } from "@/components/atoms/buttons/button";
 
 export function User() {
 	// TODO: Get user data from server's API
 
 	return (
 		<div className={styles.root}>
-			<Avatar size={40} className={styles.avatar}>
+			<Avatar size={40}>
 				<AvatarFallback>JD</AvatarFallback>
 			</Avatar>
 			<div className={styles.details}>
 				<b>John Doe</b>
-				<p className={styles.email}>john.doe@example.com</p>
 			</div>
+			<Button size="icon" level="secondary">
+				<LogOut size={16} />
+			</Button>
 		</div>
 	);
 }
