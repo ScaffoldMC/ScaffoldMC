08a91a0f1c4fbdb6f446a3c1f6ac5520aa80090e
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-27 22:04:30 -0400

    Fix testing links

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function Dashboard() {
 	return (
 		<div>
-			<Link href="/servers/1"> Server 1</Link>
-			<Link href="/servers/2"> Server 2</Link>
-			<Link href="/servers/3"> Server 3</Link>
+			<div>
+				<Link href="/dashboard/1"> Server 1</Link>
+				<Link href="/dashboard/2"> Server 2</Link>
+				<Link href="/dashboard/3"> Server 3</Link>
+			</div>
 		</div>
 	);
 }
