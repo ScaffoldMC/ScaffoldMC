f234f335e2f1de6a9e23eca2a47764e8a09fdc0a
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-27 21:33:18 -0400

    Make log output nicer

--- apps/backend/src/logger.rs
+++ apps/backend/src/logger.rs
@@ -1,3 +1,6 @@
+use std::default;
+
+use colored::Colorize;
 use log::{Level, Metadata, Record};
 
 pub struct Logger;
@@ -9,7 +12,23 @@ impl log::Log for Logger {
 
 	fn log(&self, record: &Record) {
 		if self.enabled(record.metadata()) {
-			println!("[{}] {}", record.level(), record.args());
+			let level = record.level();
+			let level_str = level.to_string();
+			let level_style = match level {
+				Level::Error => level_str.red(),
+				Level::Warn => level_str.yellow(),
+				Level::Info => level_str.blue(),
+				Level::Debug => level_str.dimmed(),
+				Level::Trace => level_str.white(),
+			}
+			.bold();
+
+			let args_str = match level {
+				Level::Debug => record.args().to_string().dimmed(),
+				_ => record.args().to_string().bright_white(),
+			};
+
+			println!("{} {}", level_style, args_str);
 		}
 	}
 
