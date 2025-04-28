e3ae5b5f3d8ea2c56c5b51f497e0fc02ec31d21d
Thurston Yates <thurstonalexyates@gmail.com>
2025-04-27 22:07:37 -0400

    Use get instead of axum::routing::get

--- apps/backend/src/routes/servers.rs
+++ apps/backend/src/routes/servers.rs
@@ -1,5 +1,5 @@
 use axum::{routing::get, Router};
 
 pub fn create_router() -> Router {
-	Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }))
+	Router::new().route("/", get(|| async { "Hello, World!" }))
 }
