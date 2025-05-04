013aa050107afed4ebb42ac5a290959a67d97aa0
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-04 00:40:10 -0400

    Create title for server

--- apps/web/app/dashboard/[slug]/page.tsx
+++ apps/web/app/dashboard/[slug]/page.tsx
@@ -1,3 +1,11 @@
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+} from "@/components/atoms/avatar/avatar";
+
+import styles from "./page.module.css";
+
 export default async function Page({
 	params,
 }: {
@@ -6,7 +14,16 @@ export default async function Page({
 	const { slug } = await params;
 	return (
 		<div>
-			<p>Server {slug}</p>
+			<div className={styles.title}>
+				<Avatar size={64} shape="square">
+					<AvatarFallback>?</AvatarFallback>
+					<AvatarImage src="/images/server-default.png" />
+				</Avatar>
+				<div className={styles.titleInfo}>
+					<h1>Server Name</h1>
+					<p>Additional info</p>
+				</div>
+			</div>
 		</div>
 	);
 }
