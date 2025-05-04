d1a599a4dcf14d49c783883951558d037875973d
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-04 02:23:38 -0400

    Create styled tabs

---
+++ apps/web/components/organisms/tabs/tabs.tsx
@@ -0,0 +1,36 @@
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import styles from "./tabs.module.css";
+
+export function Tabs({ children, ...props }: TabsPrimitive.TabsProps) {
+	return (
+		<TabsPrimitive.Root className={styles.root} {...props}>
+			{children}
+		</TabsPrimitive.Root>
+	);
+}
+
+export function TabsList({ children, ...props }: TabsPrimitive.TabsListProps) {
+	return (
+		<TabsPrimitive.List className={styles.list} {...props}>
+			{children}
+		</TabsPrimitive.List>
+	);
+}
+
+export function TabsTrigger({
+	children,
+	...props
+}: TabsPrimitive.TabsTriggerProps) {
+	return (
+		<TabsPrimitive.Trigger className={styles.trigger} {...props}>
+			{children}
+		</TabsPrimitive.Trigger>
+	);
+}
+
+export function TabsContent({
+	children,
+	...props
+}: TabsPrimitive.TabsContentProps) {
+	return <TabsPrimitive.Content {...props}>{children}</TabsPrimitive.Content>;
+}
