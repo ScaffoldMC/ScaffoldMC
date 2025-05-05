c7095279bd4abfeae6000da3e428737a09124b24
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-05 16:12:27 -0400

    Create dedicated servers route

---
+++ apps/web/app/dashboard/servers/page.tsx
@@ -0,0 +1,10 @@
+import { ServerList } from "@/components/organisms/serverlist/serverlist";
+
+export default function Servers() {
+	return (
+		<div>
+			<h1>Servers</h1>
+			<ServerList />
+		</div>
+	);
+}
