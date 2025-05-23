c1f5ed74e7174f1e2ed3e7b2b56c1d560d80b4d8
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-22 20:27:07 -0400

    Add rest of axum login logic from example

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -6,6 +6,10 @@ mod user;
 use std::net::SocketAddr;
 
 use axum::Router;
+use axum_login::{
+	tower_sessions::{MemoryStore, SessionManagerLayer},
+	AuthManagerLayerBuilder,
+};
 use log::{info, LevelFilter};
 
 static LOGGER: logger::Logger = logger::Logger;
@@ -16,7 +20,17 @@ async fn main() {
 		.map(|()| log::set_max_level(LevelFilter::Info))
 		.expect("Failed to set logger");
 
-	let app: Router = Router::new().merge(routes::create_router());
+	let db = sqlx::SqlitePool::connect("sqlite://:memory:")
+		.await
+		.unwrap();
+
+	let session_store = MemoryStore::default();
+	let session_layer = SessionManagerLayer::new(session_store);
+	let backend = backend::Backend::new(db);
+	let auth_layer = AuthManagerLayerBuilder::new(backend, session_layer).build();
+
+	let app: Router = routes::create_router().layer(auth_layer);
+
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
 
 	info!("Starting server on {}", addr);
--- apps/backend/src/routes.rs
+++ apps/backend/src/routes.rs
@@ -2,9 +2,13 @@ mod auth;
 mod servers;
 
 use axum::Router;
+use axum_login::login_required;
+
+use crate::backend::Backend;
 
 pub fn create_router() -> Router {
 	Router::new()
 		.nest("/servers", servers::create_router())
+		.route_layer(login_required!(Backend, login_url = "/auth/login"))
 		.nest("/auth", auth::create_router())
 }
