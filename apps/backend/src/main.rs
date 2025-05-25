ecb43891ea2acb89dff86fb07c501307097df4e0
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-25 00:08:37 -0400

    Create base dir if it doesnt exist

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -23,7 +23,11 @@ async fn main() {
 		.map(|()| log::set_max_level(LevelFilter::Info))
 		.expect("Failed to set logger");
 
-	let base_dir = BASE_DIR.get_or_init(|| env::current_dir().unwrap().join("/data"));
+	let base_dir = BASE_DIR.get_or_init(|| env::current_dir().unwrap().join("/data/"));
+
+	if !base_dir.exists() {
+		std::fs::create_dir_all(base_dir).expect("Failed to create base directory");
+	}
 
 	let db_connect_options = sqlx::sqlite::SqliteConnectOptions::new()
 		.filename(base_dir.join("db.sqlite"))
