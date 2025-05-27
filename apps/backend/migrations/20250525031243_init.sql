bd3501fa0ab62559f3cb46ceac97b7dcc81f76e2
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-27 15:05:50 -0400

    Add integer primary key

--- apps/backend/migrations/20250525031243_init.sql
+++ apps/backend/migrations/20250525031243_init.sql
@@ -1,10 +1,11 @@
 
 CREATE TABLE IF NOT EXISTS users (
-	id TEXT PRIMARY KEY NOT NULL,
-	name TEXT NOT NULL,
+	pk INTEGER PRIMARY KEY AUTO_INCREMENT,
+	id TEXT NOT NULL UNIQUE,
+	fullname TEXT NOT NULL,
 	username TEXT NOT NULL UNIQUE,
 	password_hash TEXT NOT NULL
 );
 
-INSERT INTO users VALUES
+INSERT INTO users (id, fullname, username, password_hash) VALUES
 	('c3f36444-b095-45fb-9fef-5c5cdffc1070', 'Test User', 'admin', '$argon2id$v=19$m=19456,t=2,p=1$efvpqQFxBnAPtAiuASeqdw$9e19BTMyZwmpb7cbBFZQwp43VMMoCuFkZS6rqKJE1OE');
