afbcea1645447a033e96b475ad954106283248f4
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-27 16:53:24 -0400

    Add logout route back
    Cookies will be used for users since they're more secure. JWT will be used in the cookies instead of as an access token.

--- apps/backend/src/routes/auth.rs
+++ apps/backend/src/routes/auth.rs
@@ -8,12 +8,19 @@ pub struct Credentials {
 }
 
 pub fn create_router() -> Router {
-	Router::new().route("/login", post(login))
+	Router::new()
+		.route("/login", post(login))
+		.route("/logout", post(logout))
 }
 
 pub async fn login(Json(creds): Json<Credentials>) -> impl IntoResponse {
 	// TODO: Login logic.
 	// Note: Use spawn_blocking for password verification.
 
+	StatusCode::OK.into_response()
+}
+
+pub async fn logout() -> impl IntoResponse {
+	// TODO: Logout logic.
 	StatusCode::OK.into_response()
 }
