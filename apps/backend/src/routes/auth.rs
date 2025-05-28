9f5e4c09986f4df3aeae7b5438649117c6a8c697
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-28 16:33:59 -0400

    Write login logic

--- apps/backend/src/routes/auth.rs
+++ apps/backend/src/routes/auth.rs
@@ -1,6 +1,12 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use log::error;
+use password_auth::{verify_password, VerifyError};
 use serde::Deserialize;
+use tokio::task::spawn_blocking;
+use tower_cookies::Cookie;
+use tower_cookies::Cookies;
 
+use crate::auth;
 use crate::db;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -16,11 +22,50 @@ pub fn create_router() -> Router<db::Database> {
 		.route("/refresh", post(refresh))
 }
 
-pub async fn login(Json(creds): Json<Credentials>) -> impl IntoResponse {
-	// TODO: Login logic.
-	// Note: Use spawn_blocking for password verification.
+pub async fn login(
+	cookies: Cookies,
+	State(db): State<db::Database>,
+	Json(creds): Json<Credentials>,
+) -> impl IntoResponse {
+	let user = db.get_user_by_username(creds.username.as_str()).await;
+	if let Err(_) = user {
+		// Handle error, e.g., user not found or database error.
+		return (StatusCode::UNAUTHORIZED, "Invalid credentials").into_response();
+	}
+	let user = user.unwrap();
 
-	StatusCode::OK.into_response()
+	let verify_result = spawn_blocking(move || {
+		verify_password(creds.password.as_str(), user.password_hash.as_str())
+	})
+	.await
+	.expect("Join error on spawn_blocking task");
+
+	if let Err(err) = verify_result {
+		return match err {
+			VerifyError::PasswordInvalid => {
+				(StatusCode::UNAUTHORIZED, "Invalid credentials").into_response()
+			}
+			VerifyError::Parse(e) => {
+				error!("Failed to parse password hash: {}", e);
+				(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error").into_response()
+			}
+		};
+	}
+
+	let auth_token = auth::create_auth_token(user.id.to_string());
+	let refresh_token = auth::create_refresh_token();
+
+	// TODO: Add refresh token to DB
+
+	let refresh_cookie = Cookie::build(("refresh_token", refresh_token))
+		.path("/")
+		.http_only(true)
+		.secure(true)
+		.same_site(tower_cookies::cookie::SameSite::Strict)
+		.build();
+
+	cookies.add(refresh_cookie);
+	(StatusCode::OK, Json(auth_token)).into_response()
 }
 
 pub async fn refresh() -> impl IntoResponse {
