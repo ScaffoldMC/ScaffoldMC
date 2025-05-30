4bba0fc940e9a491b0781c7063f5127b89427252
Thurston Yates <thurstonalexyates@gmail.com>
2025-05-30 18:47:52 -0400

    Write refresh logic

--- apps/backend/src/routes/auth.rs
+++ apps/backend/src/routes/auth.rs
@@ -1,12 +1,14 @@
+use axum::response::Response;
 use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
 use log::error;
 use password_auth::{verify_password, VerifyError};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use tokio::task::spawn_blocking;
 use tower_cookies::Cookie;
 use tower_cookies::Cookies;
 
 use crate::auth;
+use crate::auth::REFRESH_TOKEN_LENGTH;
 use crate::db;
 
 static REFRESH_COOKIE_NAME: &str = "refresh_token";
@@ -72,14 +74,58 @@ pub async fn login(
 	(StatusCode::OK, Json(auth_token)).into_response()
 }
 
+#[derive(Serialize)]
+struct RefreshResponseBody {
+	pub ref_token: String,
+	pub auth_token: String,
+}
+
 pub async fn refresh(cookies: Cookies, State(db): State<db::Database>) -> impl IntoResponse {
-	// TODO: Check token is in DB, if so then drop if not then 401
+	let ref_token = cookies
+		.get(REFRESH_COOKIE_NAME)
+		.map(|cookie| cookie.value().to_string());
 
-	let new_refresh_token = auth::create_refresh_token();
+	if ref_token.is_none() {
+		return (StatusCode::UNAUTHORIZED, "Unauthorized").into_response();
+	}
 
-	// TODO: Add refresh token to DB
+	let ref_token = ref_token.unwrap();
 
-	let new_cookie = Cookie::build((REFRESH_COOKIE_NAME, new_refresh_token))
+	let db_entry = db.get_refresh_token(&ref_token).await;
+
+	if let Err(err) = db_entry {
+		error!("Failed to get refresh token from DB: {}", err);
+		return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+	}
+
+	let db_entry = db_entry.unwrap();
+
+	if db_entry.is_none() {
+		return (StatusCode::UNAUTHORIZED, "Unauthorized").into_response();
+	}
+
+	let db_entry = db_entry.unwrap();
+
+	let current_time = time::OffsetDateTime::now_utc();
+	let token_age = current_time - db_entry.created_at;
+
+	if token_age > REFRESH_TOKEN_LENGTH {
+		db.delete_refresh_token(&ref_token)
+			.await
+			.expect("Failed to delete refresh token");
+
+		return (StatusCode::UNAUTHORIZED, "Token expired").into_response();
+	}
+
+	let auth_token = auth::create_auth_token(db_entry.user_id.to_string());
+	let new_ref_token = auth::create_refresh_token();
+
+	if let Err(err) = db.add_refresh_token(&new_ref_token, db_entry.user_id).await {
+		error!("Failed to store refresh token in DB: {}", err);
+		return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+	}
+
+	let new_cookie = Cookie::build((REFRESH_COOKIE_NAME, new_ref_token))
 		.path("/")
 		.http_only(true)
 		.secure(true)
@@ -88,7 +134,14 @@ pub async fn refresh(cookies: Cookies, State(db): State<db::Database>) -> impl I
 
 	cookies.add(new_cookie);
 
-	StatusCode::OK.into_response() // TODO: Return new auth token
+	(
+		StatusCode::OK,
+		Json(RefreshResponseBody {
+			ref_token,
+			auth_token,
+		}),
+	)
+		.into_response()
 }
 
 pub async fn logout(cookies: Cookies, State(db): State<db::Database>) -> impl IntoResponse {
