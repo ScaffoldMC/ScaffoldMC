18c942377bc0ba78749b7f8122a85b8fbaac2e7c
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-01 13:49:53 -0400

    Format uses

--- apps/backend/src/routes/servers.rs
+++ apps/backend/src/routes/servers.rs
@@ -1,8 +1,6 @@
-use std::sync::Arc;
-
-use axum::{routing::get, Router};
-
 use crate::AppState;
+use axum::{routing::get, Router};
+use std::sync::Arc;
 
 pub fn create_router() -> Router<Arc<AppState>> {
 	Router::new().route("/", get(|| async { "Hello, World!" }))
