250c455c3f2141162a07981f6476a1d595807e72
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-01 13:17:20 -0400

    Move app setup into AppState::new, add Secrets

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -2,20 +2,40 @@ mod auth;
 mod db;
 mod logger;
 mod routes;
+mod secrets;
 
-use axum::Router;
 use db::Database;
 use log::{info, LevelFilter};
-use std::sync::{Arc, OnceLock};
-use std::{env, net::SocketAddr, path::PathBuf};
-use tower_cookies::CookieManagerLayer;
+use secrets::Secrets;
+use std::sync::Arc;
+use std::{env, net::SocketAddr};
 
 static LOGGER: logger::Logger = logger::Logger;
-static BASE_DIR: OnceLock<PathBuf> = OnceLock::new();
 
 #[derive(Clone)]
 struct AppState {
 	pub db: Database,
+	pub secrets: Secrets,
+}
+
+impl AppState {
+	pub async fn new() -> Self {
+		let base_dir = env::current_dir()
+			.expect("Current dir should be accessible")
+			.join("data/");
+
+		if !base_dir.exists() {
+			std::fs::create_dir_all(&base_dir).expect("Read/write should be available");
+		}
+
+		let db = db::Database::new(&base_dir.join("db.sqlite"))
+			.await
+			.expect("Failed to start DB");
+
+		let secrets = Secrets::new(&base_dir);
+
+		AppState { db, secrets }
+	}
 }
 
 #[tokio::main]
@@ -24,17 +44,7 @@ async fn main() {
 		.map(|()| log::set_max_level(LevelFilter::Info))
 		.expect("Failed to set logger");
 
-	let base_dir = BASE_DIR.get_or_init(|| env::current_dir().unwrap().join("data/"));
-
-	if !base_dir.exists() {
-		std::fs::create_dir_all(base_dir).expect("Failed to create base directory");
-	}
-
-	let db = db::Database::new(&base_dir.join("db.sqlite"))
-		.await
-		.expect("Failed to initialize database");
-
-	let state = Arc::new(AppState { db });
+	let state = Arc::new(AppState::new().await);
 	let app = routes::create_router(state);
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
 
