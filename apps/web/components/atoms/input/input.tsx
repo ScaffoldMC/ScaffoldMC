6ca05750a30393e962319e800b8ddc4a23499697
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-04 12:13:21 -0400

    Restrict input to textinput

--- apps/web/components/atoms/input/input.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./input.module.css";
-
-export function Input({
-	...props
-}: React.InputHTMLAttributes<HTMLInputElement>) {
-	return <input className={styles.base} {...props} />;
-}
---
+++ apps/web/components/atoms/input/textinput.tsx
@@ -0,0 +1,12 @@
+import styles from "./textinput.module.css";
+
+export type TextInputProps = Omit<
+	React.InputHTMLAttributes<HTMLInputElement>,
+	"type"
+> & {
+	type?: "text" | "email" | "password" | "search" | "tel" | "url";
+};
+
+export function TextInput({ type, ...props }: TextInputProps) {
+	return <input className={styles.base} {...props} />;
+}
