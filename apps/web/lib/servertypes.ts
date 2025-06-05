ff32e32e48339d9a7d328435fa71293ca40feab4
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-05 17:15:35 -0400

    Export UserResponse

--- apps/web/lib/servertypes.ts
+++ apps/web/lib/servertypes.ts
@@ -1,3 +1,4 @@
 export * from "../../backend/bindings/LoginRequestBody";
 export * from "../../backend/bindings/LoginResponseBody";
 export * from "../../backend/bindings/RefreshResponseBody";
+export * from "../../backend/bindings/UserResponse";
