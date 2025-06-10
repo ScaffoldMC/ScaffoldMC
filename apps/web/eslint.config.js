e17c55799a9e6f2a15ec96c0bded5a33dd7e32e3
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-10 12:22:12 -0400

    Fix eslint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,8 +1,23 @@
-import next from "@next/eslint-plugin-next";
+import pluginNext from "@next/eslint-plugin-next";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginPrettier from "eslint-plugin-prettier";
+
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
 	{
-		plugins: { next },
+		plugins: {
+			"@next/next": pluginNext,
+			react: pluginReact,
+			"react-hooks": pluginReactHooks,
+			prettier: pluginPrettier,
+		},
+		rules: {
+			...pluginNext.configs.recommended.rules,
+			...pluginReact.configs.recommended.rules,
+			...pluginReactHooks.configs.recommended.rules,
+			...pluginPrettier.configs.recommended.rules,
+		},
 	},
 ]);
