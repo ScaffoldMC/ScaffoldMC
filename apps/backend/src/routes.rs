313ba7871b2cee5f766b6b83812cc1d811f069db
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-11 17:21:11 -0400

    Configure CORS

--- apps/backend/src/routes.rs
+++ apps/backend/src/routes.rs
@@ -4,16 +4,33 @@ mod servers;
 
 use crate::auth::require_auth;
 use crate::AppState;
-use axum::{middleware, Router};
+use axum::{http, middleware, Router};
 use std::sync::Arc;
 use tower_cookies::CookieManagerLayer;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_router(state: Arc<AppState>) -> Router {
+	let cors = CorsLayer::new()
+		.allow_origin(
+			"http://localhost:3000"
+				.parse::<http::HeaderValue>()
+				.unwrap(),
+		)
+		.allow_methods([
+			http::Method::GET,
+			http::Method::POST,
+			http::Method::PUT,
+			http::Method::DELETE,
+		])
+		.allow_headers([http::header::CONTENT_TYPE])
+		.allow_credentials(true);
+
 	Router::new()
 		.nest("/servers", servers::create_router())
 		.nest("/me", me::create_router())
 		.route_layer(middleware::from_fn_with_state(state.clone(), require_auth))
 		.nest("/auth", auth::create_router())
 		.layer(CookieManagerLayer::new())
+		.layer(cors)
 		.with_state(state)
 }
