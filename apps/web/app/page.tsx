9a4b43d19f5498f8dabd960534f0697bd54ba4c2
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-13 21:33:36 -0400

    Make root redirect to /home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,5 @@
 import { redirect } from "next/navigation";
 
-async function isUserLoggedIn(): Promise<boolean> {
-	// TODO: Actual authentication logic
-	return false;
-}
-
-export default async function Home() {
-	const loggedIn = await isUserLoggedIn();
-
-	if (loggedIn) {
-		redirect("/dashboard");
-	} else {
-		redirect("/login");
-	}
+export default async function Root() {
+	redirect("/home");
 }
