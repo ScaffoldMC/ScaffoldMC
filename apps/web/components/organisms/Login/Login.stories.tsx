dcc17d8bc6c4b78ade423e4940ac4bee1a85e883
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-15 18:29:37 -0400

    Create login story

---
+++ apps/web/components/organisms/Login/Login.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import { Login } from "./Login";
+
+const meta = {
+	component: Login,
+} satisfies Meta<typeof Login>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		onLogin: (username, password) => {
+			return Promise.resolve();
+		},
+	},
+};
+
+export const Invalid: Story = {
+	args: {
+		onLogin: (username, password) => {
+			return Promise.reject();
+		},
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = canvasElement as HTMLCanvasElement;
+		const usernameInput = canvas.querySelector(
+			'input[name="username"]',
+		) as HTMLInputElement;
+		const passwordInput = canvas.querySelector(
+			'input[name="password"]',
+		) as HTMLInputElement;
+		const submitButton = canvas.querySelector(
+			'button[type="submit"]',
+		) as HTMLButtonElement;
+
+		usernameInput.value = "invalidUser";
+		passwordInput.value = "invalidPass";
+
+		submitButton.click();
+	},
+};
