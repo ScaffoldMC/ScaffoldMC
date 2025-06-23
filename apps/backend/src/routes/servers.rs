a06f14731d8d9a5c055db297b8f0830fb3123d25
Thurston Yates <thurstonalexyates@gmail.com>
2025-06-22 23:00:26 -0400

    Create GET method for /servers

--- apps/backend/src/routes/servers.rs
+++ apps/backend/src/routes/servers.rs
@@ -1,7 +1,30 @@
 use crate::AppState;
-use axum::{routing::get, Router};
+use axum::{extract::State, response::IntoResponse};
+use axum::{routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
+use ts_rs::TS;
 
 pub fn create_router() -> Router<Arc<AppState>> {
-	Router::new().route("/", get(|| async { "Hello, World!" }))
+	Router::new().route("/", get(get_servers))
+}
+
+#[derive(TS, Debug, Clone, Serialize, Deserialize)]
+#[ts(export)]
+struct PartialServer {
+	id: String,
+	name: String,
+}
+
+pub async fn get_servers(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+	let servers = state
+		.servers
+		.iter()
+		.map(|server| PartialServer {
+			id: server.id.clone(),
+			name: server.name.clone(),
+		})
+		.collect::<Vec<PartialServer>>();
+
+	Json(servers).into_response()
 }
