884cea1ffb121e46420c886bdbdf625d309edbcb
Thurston Yates <thurstonalexyates@gmail.com>
2025-07-21 17:30:04 -0400

    Move struct impl to proper file

--- apps/backend/src/api/routes/me.rs
+++ apps/backend/src/api/routes/me.rs
@@ -5,16 +5,6 @@ use std::sync::Arc;
 
 use crate::api::types::user::UserResponse;
 
-impl From<User> for UserResponse {
-	fn from(db_user: User) -> Self {
-		UserResponse {
-			id: db_user.id,
-			fullname: db_user.fullname,
-			username: db_user.username,
-		}
-	}
-}
-
 pub fn create_router() -> Router<Arc<AppState>> {
 	Router::new().route("/", routing::get(get))
 }
--- apps/backend/src/api/types/user.rs
+++ apps/backend/src/api/types/user.rs
@@ -2,10 +2,22 @@ use serde::Serialize;
 use ts_rs::TS;
 use uuid::Uuid;
 
+use crate::db::user::User;
+
 #[derive(TS, Serialize)]
 #[ts(export)]
 pub struct UserResponse {
 	pub id: Uuid,
 	pub fullname: String,
 	pub username: String,
+}
+
+impl From<User> for UserResponse {
+	fn from(db_user: User) -> Self {
+		UserResponse {
+			id: db_user.id,
+			fullname: db_user.fullname,
+			username: db_user.username,
+		}
+	}
 }
