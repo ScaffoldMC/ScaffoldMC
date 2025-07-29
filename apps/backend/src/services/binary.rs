cf1e66ce53d4d3d3a4685e3b9fe6b53480f8e0c9
Thurston Yates <thurstonalexyates@gmail.com>
2025-07-29 16:02:55 -0400

    Use lockfile in binary service

--- apps/backend/src/services/binary.rs
+++ apps/backend/src/services/binary.rs
@@ -1,11 +1,28 @@
-use reqwest::Url;
-
 use crate::core::bin_providers::{
 	fabric::FabricBinaryProvider, mojang_java::MojangJavaBinaryProvider, BinaryProvider,
 };
 use crate::core::config::game::Game;
 use crate::services::Service;
+use crate::util::hash::{compute_file_hash, HashAlgorithm};
+use reqwest::Url;
+use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
 use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BinaryLockfile {
+	version: String,
+	binaries: HashMap<String, BinaryLockfileEntry>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BinaryLockfileEntry {
+	game: Game,
+	path: PathBuf,
+	hash: String,
+	hash_algorithm: HashAlgorithm,
+}
 
 pub struct BinaryService {
 	binaries_dir: String,
@@ -35,11 +52,19 @@ impl BinaryService {
 
 	/// Retrieves a list of available games.
 	pub async fn get_games(&self) -> Result<Vec<Game>, String> {
-		todo!("Implement game retrieval logic");
+		let lockfile = self.load_lockfile().await?;
+
+		let games = lockfile
+			.binaries
+			.values()
+			.map(|entry| entry.game.clone())
+			.collect();
+
+		Ok(games)
 	}
 
 	/// Installs a game with the specified configuration.
-	pub async fn install_game(&self, game: Game) -> Result<(), String> {
+	pub async fn install_game(&self, game: Game) -> Result<PathBuf, String> {
 		let binary_dir = self.binary_dir(&game);
 
 		// Ensure the binary directory exists
@@ -54,27 +79,49 @@ impl BinaryService {
 		let binary_name = provider.binary_name();
 		let binary_path = binary_dir.join(binary_name);
 
-		Self::download_file(download_url, binary_path)
+		Self::download_file(download_url, binary_path.clone())
 			.await
 			.map_err(|e| format!("Failed to download game: {}", e))?;
 
-		Ok(())
+		// Add binary to the lockfile
+		let mut lockfile = self
+			.load_lockfile()
+			.await
+			.map_err(|e| format!("Failed to load lockfile: {}", e))?;
+
+		let lockfile_entry = BinaryLockfileEntry {
+			game: game.clone(),
+			path: binary_path.clone(),
+			hash: String::new(), // TODO: Get hash
+			hash_algorithm: HashAlgorithm::Sha256,
+		};
+
+		lockfile
+			.binaries
+			.insert(game.identifier().to_string(), lockfile_entry);
+
+		self.save_lockfile(&lockfile).await?;
+
+		Ok(binary_path)
 	}
 
 	/// Returns the path to the binary directory for a given game, if not
 	/// available it will be downloaded.
 	pub async fn ensure_binary(&self, game: &Game) -> Result<PathBuf, String> {
-		let provider = self.get_provider(game);
-		let binary_name = provider.binary_name();
-		let binary_path = self.binary_dir(game).join(binary_name);
+		let lockfile = self.load_lockfile().await?;
 
-		if !binary_path.exists() {
-			self.install_game(game.clone())
-				.await
-				.map_err(|e| format!("Failed to ensure binary: {}", e))?;
+		if let Some(entry) = lockfile.binaries.get(game.identifier()) {
+			if self.validate_binary(entry).await.is_ok() {
+				return Ok(entry.path.clone());
+			}
 		}
 
-		Ok(binary_path)
+		let binary_path = self
+			.install_game(game.clone())
+			.await
+			.map_err(|e| format!("Failed to ensure binary: {}", e))?;
+
+		return Ok(binary_path);
 	}
 
 	/// Returns a list of installed games.
@@ -150,6 +197,58 @@ impl BinaryService {
 			.map_err(|e| format!("Failed to read response: {}", e))?;
 
 		std::fs::write(path, bytes).map_err(|e| format!("Failed to save file: {}", e))?;
+
+		Ok(())
+	}
+
+	async fn load_lockfile(&self) -> Result<BinaryLockfile, String> {
+		let lockfile_path = PathBuf::from(format!("{}/binary.lock", &self.binaries_dir));
+
+		if !lockfile_path.exists() {
+			return Err("Binary lockfile does not exist".to_string());
+		}
+
+		let file_content = tokio::fs::read_to_string(lockfile_path)
+			.await
+			.map_err(|e| e.to_string())?;
+
+		let lockfile: BinaryLockfile = toml::from_str(&file_content).map_err(|e| e.to_string())?;
+
+		Ok(lockfile)
+	}
+
+	async fn save_lockfile(&self, lockfile: &BinaryLockfile) -> Result<(), String> {
+		let lockfile_path = PathBuf::from(format!("{}/binary.lock", &self.binaries_dir));
+
+		let file_content = toml::to_string(lockfile).map_err(|e| e.to_string())?;
+
+		tokio::fs::write(lockfile_path, file_content)
+			.await
+			.map_err(|e| e.to_string())?;
+
+		Ok(())
+	}
+
+	async fn validate_binary(&self, entry: &BinaryLockfileEntry) -> Result<(), String> {
+		if !entry.path.exists() {
+			return Err(format!("Binary path does not exist: {:?}", entry.path));
+		}
+
+		let entry_path = entry.path.clone();
+		let algorithm = entry.hash_algorithm;
+
+		let result = tokio::task::spawn_blocking(move || compute_file_hash(algorithm, &entry_path))
+			.await
+			.map_err(|e| e.to_string())?;
+
+		let file_hash = result?;
+
+		if file_hash != entry.hash {
+			return Err(format!(
+				"Binary hash mismatch for {:?}: expected {}, got {}",
+				entry.path, entry.hash, file_hash
+			));
+		}
 
 		Ok(())
 	}
