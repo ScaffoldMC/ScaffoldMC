72df6bc4c5c02094d946f2d7fda07e448e1e5ee9
Thurston Yates <thurstonalexyates@gmail.com>
2025-11-11 20:25:34 -0500

    Update bindings import

--- apps/web/lib/servertypes.ts
+++ apps/web/lib/servertypes.ts
@@ -2,3 +2,6 @@ export * from "../../backend/bindings/LoginRequest";
 export * from "../../backend/bindings/LoginResponse";
 export * from "../../backend/bindings/RefreshResponse";
 export * from "../../backend/bindings/UserResponse";
+export * from "../../backend/bindings/PartialServer";
+export * from "../../backend/bindings/SudoRequest";
+export * from "../../backend/bindings/UserPatchRequest";
