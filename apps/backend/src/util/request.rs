76b017816bd720207ba81fa208071cb222064562
Thurston Yates <thurstonalexyates@gmail.com>
2025-12-30 17:16:36 -0600

    Make get_and_format return richer errors

--- apps/backend/src/util/request.rs
+++ apps/backend/src/util/request.rs
@@ -8,10 +8,15 @@ pub async fn get_and_format<U: IntoUrl, T: serde::de::DeserializeOwned>(
 		.get(url)
 		.send()
 		.await
-		.map_err(|e| format!("Failed to fetch versions: {}", e))?
-		.json::<T>()
-		.await
-		.map_err(|e| format!("Failed to parse response: {}", e))?;
+		.map_err(|e| format!("Failed to fetch versions: {}", e))?;
 
-	Ok(response)
+	let text = response
+		.text()
+		.await
+		.map_err(|e| format!("Failed to read response body: {}", e))?;
+
+	let decoded: T =
+		serde_json::from_str(&text).map_err(|e| format!("Failed to parse response: {}", e))?;
+
+	Ok(decoded)
 }
