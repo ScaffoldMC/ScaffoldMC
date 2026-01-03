4ec0111f96d5440cead1de0c8beb54b4a1700c81
Thurston Yates <thurstonalexyates@gmail.com>
2026-01-02 22:35:38 -0600

    Add to create server dialog

--- apps/web/components/organisms/ServerCreateDialog/ServerCreateDialog.tsx
+++ apps/web/components/organisms/ServerCreateDialog/ServerCreateDialog.tsx
@@ -2,6 +2,10 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { DialogContent, DialogOverlay, DialogPortal } from "../Dialog/Dialog";
 import { VersionSelector } from "../VersionSelector/VersionSelector";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { Label } from "@/components/atoms/Label/Label";
+import { TextInput } from "@/components/atoms/TextInput/TextInput";
+import styles from "./ServerCreateDialog.module.css";
+import { Button } from "@/components/atoms/Button/Button";
 
 export function ServerCreateDialogPortal() {
 	return (
@@ -11,10 +15,27 @@ export function ServerCreateDialogPortal() {
 				<DialogPrimitive.Title>Create Server</DialogPrimitive.Title>
 				<VisuallyHidden>
 					<DialogPrimitive.Description>
-						Select the edition and version for your new server.
+						Select the software and configure your new server.
 					</DialogPrimitive.Description>
 				</VisuallyHidden>
-				<VersionSelector />
+				<form className={styles.form}>
+					<div className={styles.field}>
+						<Label>Server Software</Label>
+						<VersionSelector />
+					</div>
+					<div className={styles.field}>
+						<Label>Server Name</Label>
+						<TextInput />
+					</div>
+					<Button type="submit" level="primary">
+						Create
+					</Button>
+					<DialogPrimitive.Close asChild>
+						<Button type="button" level="secondary">
+							Cancel
+						</Button>
+					</DialogPrimitive.Close>
+				</form>
 			</DialogContent>
 		</DialogPortal>
 	);
