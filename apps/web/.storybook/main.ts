e974b62d9d5837cf578bba2fac5ec829595747b4
Thurston Yates <thurstonalexyates@gmail.com>
2026-03-19 17:42:04 -0400

    Make storybook work

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,10 +1,12 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
 	stories: ["../**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
-	addons: [],
+	addons: [getAbsolutePath("@storybook/addon-themes")],
 	framework: {
-		name: "@storybook/nextjs-vite",
+		name: getAbsolutePath("@storybook/nextjs-vite"),
 		options: {},
 	},
 };
@@ -14,3 +16,7 @@ export default config;
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+	return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
