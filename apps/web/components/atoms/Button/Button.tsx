ff44bbe42448e6ac90f88fce7a82866f145e1288
Thurston Yates <thurstonalexyates@gmail.com>
2026-03-19 23:37:35 -0400

    Rewrite button with tailwind

--- apps/web/components/atoms/Button/Button.tsx
+++ apps/web/components/atoms/Button/Button.tsx
@@ -2,33 +2,61 @@
 
 import { ButtonHTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
-import styles from "./Button.module.css";
+import { cn } from "@/lib/util";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	size?: "default" | "variable";
 	level?: "primary" | "secondary" | "destructive" | "ghost";
 }
 
-const buttonStyles = cva(styles.base, {
-	variants: {
-		level: {
-			primary: styles.primary,
-			secondary: styles.secondary,
-			destructive: styles.destructive,
-			ghost: styles.ghost,
+const buttonStyles = cva(
+	cn(
+		"inline-flex items-center justify-center font-semibold cursor-pointer",
+		"h-9 w-fit gap-2 px-2 border-0 rounded-(--border-radius)",
+		"text-(--font-size-medium) transition-[background-color] duration-100 ease-in-out",
+		"text-(--color-text) dark:text-white",
+		"[&_svg]:inline-block disabled:cursor-not-allowed disabled:opacity-50",
+	),
+	{
+		variants: {
+			level: {
+				primary:
+					"bg-primary text-white enabled:hover:bg-[hsl(from_var(--color-primary)_h_s_calc(l-10))]",
+				secondary: cn(
+					"bg-gray-100 enabled:hover:bg-gray-200",
+					"dark:bg-gray-900 dark:enabled:hover:bg-gray-800",
+				),
+				destructive: cn(
+					"bg-red-200  text-red-900 hover:enabled:bg-red-300",
+					"dark:bg-red-900 dark:text-red-300 dark:hover:enabled:bg-red-800",
+				),
+				ghost: cn(
+					"bg-transparent enabled:hover:bg-[hsl(from_var(--color-secondary)_h_s_calc(l-10))]",
+					"dark:enabled:hover:bg-[hsl(from_var(--color-secondary)_h_s_calc(l+10))]",
+				),
+			},
+			size: {
+				default: "h-9 min-w-9",
+				variable: "h-fit px-2 py-0",
+			},
 		},
-		size: {
-			default: styles.defaultSize,
-			variable: styles.variableSize,
+		defaultVariants: {
+			level: "secondary",
+			size: "default",
 		},
 	},
-	defaultVariants: {
-		level: "secondary",
-		size: "default",
-	},
-});
+);
 
-export function Button({ level, size, ...props }: ButtonProps) {
-	const className = buttonStyles({ level, size });
-	return <button className={className} {...props}></button>;
+export function Button({
+	level,
+	size,
+	className,
+	children,
+	...props
+}: ButtonProps) {
+	return (
+		<button className={buttonStyles({ level, size, className })} {...props}>
+			{children}
+		</button>
+	);
 }
