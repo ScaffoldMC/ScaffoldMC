7e99dba74ba0526262c600f9b6d33b9645da581d
Thurston Yates <thurstonalexyates@gmail.com>
2026-04-03 11:48:52 -0400

    Create common page layout

--- apps/web/app/(protected)/(with-sidebar)/home/page.tsx
+++ apps/web/app/(protected)/(with-sidebar)/home/page.tsx
@@ -1,13 +1,14 @@
+import PageLayout from "@/components/atoms/PageLayout/PageLayout";
 import styles from "./page.module.css";
 import { ServerList } from "@/components/organisms/ServerList/ServerList";
 
 export default function Dashboard() {
 	return (
-		<div className={styles.dashboard}>
+		<PageLayout>
 			<h1>Hello John Doe</h1>
 			<div className={styles.dashboardGrid}>
 				<ServerList />
 			</div>
-		</div>
+		</PageLayout>
 	);
 }
