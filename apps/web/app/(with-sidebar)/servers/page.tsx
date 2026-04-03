873e1fbe7513abe8d08bd4082d17448b412fb027
Thurston Yates <thurstonalexyates@gmail.com>
2026-04-02 22:41:14 -0400

    Replace create-server page with ServerCreateDialog

--- apps/web/app/(with-sidebar)/servers/page.tsx
+++ apps/web/app/(with-sidebar)/servers/page.tsx
@@ -1,20 +1,34 @@
 "use client";
 
 import { Button } from "@/components/atoms/Button/Button";
+import { DialogRoot } from "@/components/organisms/Dialog/Dialog";
+import { ServerCreateDialog } from "@/components/organisms/ServerCreateDialog/ServerCreateDialog";
 import { ServerList } from "@/components/organisms/ServerList/ServerList";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Servers() {
 	const router = useRouter();
+	const [createDialogOpen, setCreateDialogOpen] = useState(false);
 
-	const handleClick = () => {
-		router.push("/create-server");
+	const handleServerCreate = () => {
+		setCreateDialogOpen(false);
 	};
 
 	return (
 		<div>
 			<h1>Servers</h1>
-			<Button onClick={handleClick}>Create Server</Button>
+			<DialogRoot
+				open={createDialogOpen}
+				modal={true}
+				onOpenChange={(open) => setCreateDialogOpen(open)}
+			>
+				<ServerCreateDialog onServerCreate={handleServerCreate} />
+			</DialogRoot>
+
+			<Button onClick={() => setCreateDialogOpen(true)}>
+				Create Server
+			</Button>
 			<ServerList />
 		</div>
 	);
