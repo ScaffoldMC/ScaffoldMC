f2246927b132dec50c4bff24777ed826f42d2262
Thurston Yates <thurstonalexyates@gmail.com>
2026-04-17 16:28:27 -0400

    Improve warning appearence

--- apps/web/components/molecules/Alert/Alert.tsx
+++ apps/web/components/molecules/Alert/Alert.tsx
@@ -7,15 +7,21 @@ interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export function Alert({ children, type }: AlertProps) {
-	const className = cva(cn("rounded-lg border p-2"), {
-		variants: {
-			type: {
-				info: "border-accent-base bg-accent-lightest text-accent-darkest",
-				warning: "border-brand bg-brand/25 text-text-primary",
-				error: "border-red-500 bg-red-100 text-red-900",
+	const className = cva(
+		cn("flex flex-row gap-2 items-center rounded-lg border p-2"),
+		{
+			variants: {
+				type: {
+					info: "border-accent-base bg-accent-lightest text-accent-darkest",
+					warning: cn(
+						"border-yellow-500 bg-yellow-100 text-yellow-900",
+						"dark:border-yellow-500 dark:bg-yellow-900/10 dark:text-yellow-300",
+					),
+					error: "border-red-500 bg-red-100 text-red-900",
+				},
 			},
 		},
-	})({ type });
+	)({ type });
 
 	return <div className={className}>{children}</div>;
 }
