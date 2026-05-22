82df24ddae2b687ea2994bf56013bcc27679a375
Thurston Yates <thurstonalexyates@gmail.com>
2026-05-21 23:14:47 -0400

    Adjust link appearence

--- apps/web/components/organisms/Navbar/Navbar.tsx
+++ apps/web/components/organisms/Navbar/Navbar.tsx
@@ -5,13 +5,12 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { cva } from "class-variance-authority";
 import { cn } from "@/lib/util";
-import { Server, Settings } from "lucide-react";
 import { User } from "@/components/molecules/User/User";
 import Image from "next/image";
 
 export function Navbar({ children }: { children: React.ReactNode }) {
 	return (
-		<nav className="flex h-16 flex-row border-b border-border-static bg-surface sticky top-0">
+		<nav className="flex h-14 flex-row border-b border-border-static bg-surface sticky top-0">
 			<div className="flex items-center w-16 mx-3 bg-orange">
 				<Image
 					src="/images/logo.svg"
@@ -30,19 +29,16 @@ export function Navbar({ children }: { children: React.ReactNode }) {
 	);
 }
 
-const activeClassName = cva(
+const buttonClassName = cva(
 	cn(
-		"flex flex-row items-center justify-start gap-2 rounded-md border border-transparent p-2 no-underline",
+		"flex flex-row items-center justify-start gap-1.5 p-2 no-underline",
 		"transition-[background-color,border-color,color] duration-100 ease-in-out",
 	),
 	{
 		variants: {
 			active: {
-				true: cn(
-					"bg-accent-lightest text-accent-darkest hover:bg-accent-lightest",
-					"dark:bg-accent-darkest dark:text-accent-lightest dark:hover:bg-accent-darkest",
-				),
-				false: "hover:bg-surface-overlay text-text-primary",
+				true: "text-brand",
+				false: "hover:text-text-primary text-text-secondary border-transparent",
 			},
 		},
 		defaultVariants: {
@@ -59,7 +55,7 @@ export function NavbarLink({
 } & LinkProps) {
 	const pathname = usePathname();
 	const [isActive, setIsActive] = useState(false);
-	const className = activeClassName({ active: isActive });
+	const className = buttonClassName({ active: isActive });
 
 	useEffect(() => {
 		setIsActive(pathname === props.href.toString());
