951799d789604dbe172b259bb439346034496246
Thurston Yates <thurstonalexyates@gmail.com>
2026-05-23 16:27:51 -0400

    Restructure files api routes

--- apps/backend/src/api/routes/servers/_id/files.rs
+++ apps/backend/src/api/routes/servers/_id/files.rs
@@ -4,13 +4,14 @@ use axum::{routing, Router};
 use reqwest::StatusCode;
 use std::sync::Arc;
 
-mod _path;
-
 pub fn create_router() -> Router<Arc<AppState>> {
-	Router::new()
-		.route("/", routing::get(get).post(post))
-		.nest("/{path}", _path::create_router())
+	Router::new().route(
+		"/{*path}",
+		routing::get(get).post(post).delete(delete).put(put),
+	)
 }
+
+// TODO: Prevent path traversal attacks in all routes!!!
 
 async fn post() -> impl IntoResponse {
 	// TODO: Create a new file
@@ -19,5 +20,15 @@ async fn post() -> impl IntoResponse {
 
 async fn get() -> impl IntoResponse {
 	// TODO: Get file listings
+	StatusCode::OK.into_response()
+}
+
+async fn delete() -> impl IntoResponse {
+	// TODO: Delete a file
+	StatusCode::NO_CONTENT.into_response()
+}
+
+async fn put() -> impl IntoResponse {
+	// TODO: Update a file contents
 	StatusCode::OK.into_response()
 }
