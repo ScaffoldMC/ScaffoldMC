baf58ebcec505270482d3751a67819c33caefb37
Thurston Yates <thurstonalexyates@gmail.com>
2026-06-14 10:25:35 -0400

    Make structs serializable

--- apps/backend/src/models/file_manager/types.rs
+++ apps/backend/src/models/file_manager/types.rs
@@ -1,21 +1,33 @@
-use std::ffi::OsString;
+use serde::Serialize;
+use thiserror::Error;
+use ts_rs::TS;
 
+#[derive(TS, Debug, Serialize)]
+#[ts(export)]
 pub struct FSDirectoryEntry {
-	pub name: OsString,
+	pub name: String,
 }
 
+#[derive(TS, Debug, Serialize)]
+#[ts(export)]
 pub struct FSFileEntry {
-	pub name: OsString,
+	pub name: String,
 	pub size: u64,
 }
 
+#[derive(TS, Debug, Serialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
 pub enum FSEntry {
 	File(FSFileEntry),
 	Dir(FSDirectoryEntry),
 }
 
+#[derive(Debug, Error)]
 pub enum FileManagerError {
+	#[error("Permission denied")]
 	PermissionDenied,
+	#[error("Unknown file type")]
 	UnknownType,
+	#[error("I/O error: {0}")]
 	IoError(std::io::Error),
 }
